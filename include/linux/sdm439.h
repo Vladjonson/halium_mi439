7889f455916a65459c1e1a7a189048115efcc292
Vladjonson <brodovii.vladislav@gmail.com>
2023-02-18 08:45:10 +0200

    first commit

---
+++ include/linux/sdm439.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+// Author: @AkiraNoSushi
+
+#ifndef _LINUX_SDM439_H
+#define _LINUX_SDM439_H
+
+enum SDM439_devices {
+    DEVICE_UNKNOWN = -1,
+    XIAOMI_PINE,
+    XIAOMI_OLIVES
+};
+
+extern enum SDM439_devices sdm439_devices;
+
+extern int sdm439_current_device;
+
+extern bool prebuilt_camera_hal;
+
+extern bool plain_partitions;
+
+#endif
